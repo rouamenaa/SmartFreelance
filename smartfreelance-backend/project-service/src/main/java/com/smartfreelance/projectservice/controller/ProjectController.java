08ae011a2de5cd4ae0caca2f3ce13dc00347ccbe
Roua <roua.menaa@gmail.com>
2026-02-28 17:12:59 +0100

    modif api gateway

--- smartfreelance-backend/project-service/src/main/java/com/smartfreelance/projectservice/controller/ProjectController.java
+++ smartfreelance-backend/project-service/src/main/java/com/smartfreelance/projectservice/controller/ProjectController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/projects")
 public class ProjectController {
 
