e4c586de737050d47c0f427d8ca18bd356f0d0e0
Roua <roua.menaa@gmail.com>
2026-04-22 20:12:50 +0100

    trigger pipeline

--- smartfreelance-backend/project-service/src/test/java/com/smartfreelance/projectservice/controller/AuditControllerIntegrationTest.java
+++ smartfreelance-backend/project-service/src/test/java/com/smartfreelance/projectservice/controller/AuditControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
 class AuditControllerIntegrationTest {
 
--- smartfreelance-backend/project-service/src/test/java/com/smartfreelance/projectservice/controller/TaskControllerIntegrationTest.java
+++ smartfreelance-backend/project-service/src/test/java/com/smartfreelance/projectservice/controller/TaskControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
 class TaskControllerIntegrationTest {
 
